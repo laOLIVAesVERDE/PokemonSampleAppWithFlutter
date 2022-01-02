e1a3f0491a461e7a45205bcfd7de69db1429b851
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-02 16:56:15 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_sample_app/poke_detail.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TopPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,53 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-                    height: 100,
-                    width: 100,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text(
-                    "No.25",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                )
-              ],
-            ),
-            const Text(
-              "pikachu",
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            const Chip(
-              label: Text("electric"),
-              backgroundColor: Colors.yellow,
-            )
-          ]
-        )
-      ),
+      home: const PokeDetail(),
     );
   }
 }
