08956ceb2521dc948ec187da1165c6380c0ff63c
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-02 17:17:33 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pokemon_sample_app/poke_detail.dart';
+import './poke_detail.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
 
 class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
