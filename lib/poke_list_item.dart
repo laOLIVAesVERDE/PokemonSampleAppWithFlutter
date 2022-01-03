1153760a189b5849a78547f09fe7aef650cc65d0
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-03 21:25:13 +0900

    Create poke_list_item.dart

---
+++ lib/poke_list_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
