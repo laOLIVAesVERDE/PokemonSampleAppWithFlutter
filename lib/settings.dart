5c34d75a3e57252cbde678d076e4302c9b1c8e7d
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-04 17:13:54 +0900

    Update settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pokemon_sample_app/theme_mode_selection_page.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  ThemeMode _themeMode = ThemeMode.system;
   @override
   Widget build(BuildContext context) {
     return ListView(
