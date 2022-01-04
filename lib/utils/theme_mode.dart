4dfbc0a6212b303a26ba2ffb453e79b3f74c51b4
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-04 19:24:12 +0900

    Update theme_mode.dart

--- lib/utils/theme_mode.dart
+++ lib/utils/theme_mode.dart
@@ -4,7 +4,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 const defaultTheme = ThemeMode.system;
 
 Future<void> saveThemeMode(ThemeMode mode) async {
+  final pref = await SharedPreferences.getInstance();
+  pref.setString(mode.key, mode.name);
+}
 
+Future<ThemeMode> loadThemeMode() async {
+  final pref = await SharedPreferences.getInstance();
+  String? modeStr = pref.getString(defaultTheme.key);
+  return toMode(modeStr ?? defaultTheme.name);
+}
+
+ThemeMode toMode(String str) {
+  return ThemeMode.values.where((value) => value.name == str).first;
 }
 
 /// ThemeModeの拡張関数
