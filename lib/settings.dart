467af5f8b5e82ae53d317943fd0df519afdf9f7b
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-04 15:35:55 +0900

    Create settings.dart

---
+++ lib/settings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        ListTile(
+          leading: Icon(Icons.lightbulb),
+          title: Text("Dark/Light Mode"),
+        )
+      ],
+    );
+  }
+}
