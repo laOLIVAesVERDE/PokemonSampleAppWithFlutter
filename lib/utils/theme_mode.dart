019f57c871d15b2976147a8f15ccd346f0f092a1
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-04 19:14:06 +0900

    update

---
+++ lib/utils/theme_mode.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const defaultTheme = ThemeMode.system;
+
+Future<void> saveThemeMode(ThemeMode mode) async {
+  
+}
