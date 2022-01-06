83e3c227ed1c582dc576fa0d30e8200eff7572b4
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-06 21:30:54 +0900

    Update theme_mode_notifier.dart

--- lib/models/theme_mode_notifier.dart
+++ lib/models/theme_mode_notifier.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import '../utils/theme_mode.dart';
 
 class ThemeModeNotifier extends ChangeNotifier {
-  
+  late ThemeMode _themeMode;
+  ThemeMode get mode => _themeMode;
+  ThemeModeNotifier() {
+
+  }
 }
