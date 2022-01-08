5b73685b3d49da31fd965b56855ccfbb299cb8b3
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-08 19:18:01 +0900

    Create pokemon.dart

---
+++ lib/models/pokemon.dart
@@ -0,0 +1,13 @@
+class Pokemon {
+  final int id;
+  final String name;
+  final List<String> types;
+  final String imageUrl;
+
+  Pokemon({
+    required this.id,
+    required this.name,
+    required this.types,
+    required this.imageUrl
+  })
+}
