05aa5716fbe7d266d9c9a971e50efc8fda3119a4
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-08 20:09:38 +0900

    Update poke_api.dart

--- lib/api/poke_api.dart
+++ lib/api/poke_api.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+import 'package:pokemon_sample_app/constants/api_constants.dart';
 import 'package:pokemon_sample_app/models/pokemon.dart';
 import 'package:http/http.dart' as http;
 
 Future<Pokemon> fetchPokemon(int id) async {
-  final result = await http.get(url)
+  final response = await http.get(Uri.parse("$baseUrl/pokemon/$id"));
+  if (response.statusCode == 200) {
+    return Pokemon.fromJson(json.decode(response.body));
+  } else {
+    throw Exception("Failed to Load Pokemon");
+  }
 }
