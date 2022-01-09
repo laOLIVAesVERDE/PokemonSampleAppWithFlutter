50d2490bea93a3b94cdbc007f081a1d53201b3ed
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-09 23:52:14 +0900

    Update pokemons_notifier.dart

--- lib/models/pokemons_notifier.dart
+++ lib/models/pokemons_notifier.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:pokemon_sample_app/models/pokemon.dart';
 
 class PokemonsNotifier extends ChangeNotifier {
-
+  final Map<int, Pokemon> _pokeMap = {};
+  Map<int, Pokemon> get pokes => _pokeMap;
 }
