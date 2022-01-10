8cf855072199402f70b57b95f1353b16a8ecc963
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-10 17:49:16 +0900

    Update poke_list.dart

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -3,9 +3,14 @@ import 'package:pokemon_sample_app/models/pokemons_notifier.dart';
 import 'package:provider/provider.dart';
 import './poke_list_item.dart';
 
-class PokeList extends StatelessWidget {
+class PokeList extends StatefulWidget {
   const PokeList({Key? key}) : super(key: key);
 
+  @override
+  State<PokeList> createState() => _PokeListState();
+}
+
+class _PokeListState extends State<PokeList> {
   @override
   Widget build(BuildContext context) {
     return Consumer<PokemonsNotifier>(
