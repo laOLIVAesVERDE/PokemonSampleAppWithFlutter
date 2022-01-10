7967fd5ccae9f06564bfa2daafbc954efcfda6cc
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-10 18:37:26 +0900

    Update poke_detail.dart

--- lib/poke_detail.dart
+++ lib/poke_detail.dart
@@ -9,58 +9,64 @@ class PokeDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+            Stack(
               children: [
-                Stack(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(32),
-                      child: Image.network(
-                        pokemon.imageUrl,
-                        height: 100,
-                        width: 100,
-                      ),
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    pokemon.imageUrl,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    "No.${pokemon.id}",
+                    style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(
-                        "No.${pokemon.id}",
-                        style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Text(
-                  pokemon.name,
-                  style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  // PaddingWidgetのリストを渡す
-                  children: pokemon.types.map((type) =>
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: Chip(
-                          backgroundColor: pokeTypeColors[type] ?? Colors.grey,
-                          label: Text(
-                            type,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: _getTextLuminanceBy(type) > 0.5 ? Colors.black : Colors.white,
-                            ),
-                          ),
-                        )
-                      )
-                  ).toList(),
+                  ),
                 )
-              ]
-          )
-      ),
+              ],
+            ),
+            Text(
+              pokemon.name,
+              style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // PaddingWidgetのリストを渡す
+              children: pokemon.types.map((type) =>
+                  Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Chip(
+                        backgroundColor: pokeTypeColors[type] ?? Colors.grey,
+                        label: Text(
+                          type,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: _getTextLuminanceBy(type) > 0.5 ? Colors.black : Colors.white,
+                          ),
+                        ),
+                      )
+                  )
+              ).toList(),
+            )
+          ],
+        ),
+      )
     );
   }
 
