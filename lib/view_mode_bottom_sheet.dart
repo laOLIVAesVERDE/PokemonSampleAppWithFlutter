6d9fe3ea40a22c449f605d1ec289513ddb558dd1
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-12 23:00:47 +0900

    Update view_mode_bottom_sheet.dart

--- lib/view_mode_bottom_sheet.dart
+++ lib/view_mode_bottom_sheet.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class ViewModeBottomSheet extends StatelessWidget {
   const ViewModeBottomSheet({Key? key, required this.isFavorite}) : super(key: key);
   final bool isFavorite;
+
+  String mainText(bool isFavorite) {
+    return isFavorite ? 'お気に入りのポケモンが表示されています' : 'すべてのポケモンが表示されています';
+  }
+  String menuTitle(bool isFavorite) {
+    return isFavorite ? '「すべて」表示に切り替え' : '「お気に入り」表示に切り替え';
+  }
+  String menuSubTitle(bool isFavorite) {
+    return isFavorite ? '全てのポケモンが表示されます' : 'お気に入りに登録したポケモンのみが表示されます';
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
