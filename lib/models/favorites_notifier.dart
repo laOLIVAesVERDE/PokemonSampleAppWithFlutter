c2bd6a15b9afd8b28d7527413a5aa2c722f70381
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-13 23:41:42 +0900

    Update favorites_notifier.dart

--- lib/models/favorites_notifier.dart
+++ lib/models/favorites_notifier.dart
@@ -5,6 +5,12 @@ class FavoritesNotifier extends ChangeNotifier {
   final List<Favorite> _favorites = [];
   List<Favorite> get favorites => _favorites;
 
+  void toggle(Favorite favorite) {
+    isExist(favorite.pokeId) ? delete(favorite) : add(favorite);
+  }
+
+  bool isExist(int pokeId) => (_favorites.indexWhere((it) => it.pokeId == pokeId) >= 0) ? true : false;
+
   void add(Favorite favorite) {
     _favorites.add(favorite);
     notifyListeners();
