c1b46ffe279e0b1e6acbe6fe6a511182e2a5ce07
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-13 23:31:42 +0900

    Update poke_detail.dart

--- lib/poke_detail.dart
+++ lib/poke_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_sample_app/models/favorites_notifier.dart';
+import 'package:provider/provider.dart';
 
 import 'constants/api_constants.dart';
 import 'models/pokemon.dart';
@@ -8,67 +10,69 @@ class PokeDetail extends StatelessWidget {
   final Pokemon pokemon;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListTile(
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ),
-            const Spacer(),
-            Stack(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    pokemon.imageUrl,
-                    height: 100,
-                    width: 100,
-                  ),
+    return Consumer<FavoritesNotifier>(
+      builder: (context, favoritesNotifier, child) => Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ListTile(
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context),
                 ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    "No.${pokemon.id}",
-                    style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold
+              ),
+              const Spacer(),
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(32),
+                    child: Image.network(
+                      pokemon.imageUrl,
+                      height: 100,
+                      width: 100,
                     ),
                   ),
-                )
-              ],
-            ),
-            Text(
-              pokemon.name,
-              style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // PaddingWidgetのリストを渡す
-              children: pokemon.types.map((type) =>
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: Chip(
-                        backgroundColor: pokeTypeColors[type] ?? Colors.grey,
-                        label: Text(
-                          type,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: _getTextLuminanceBy(type) > 0.5 ? Colors.black : Colors.white,
-                          ),
-                        ),
-                      )
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      "No.${pokemon.id}",
+                      style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
                   )
-              ).toList(),
-            ),
-            const Spacer(),
-          ],
-        ),
-      )
+                ],
+              ),
+              Text(
+                pokemon.name,
+                style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // PaddingWidgetのリストを渡す
+                children: pokemon.types.map((type) =>
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: Chip(
+                          backgroundColor: pokeTypeColors[type] ?? Colors.grey,
+                          label: Text(
+                            type,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: _getTextLuminanceBy(type) > 0.5 ? Colors.black : Colors.white,
+                            ),
+                          ),
+                        )
+                    )
+                ).toList(),
+              ),
+              const Spacer(),
+            ],
+          ),
+        )
+      ),
     );
   }
 
