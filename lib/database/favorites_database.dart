0e673dd653a01b2947b07d8824dba23f37de93ba
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-15 20:32:24 +0900

    Update favorites_database.dart

--- lib/database/favorites_database.dart
+++ lib/database/favorites_database.dart
@@ -1,15 +1,16 @@
 import 'package:path/path.dart';
+import 'package:pokemon_sample_app/constants/database_constants.dart';
 import 'package:sqflite/sqflite.dart';
 
 class FavoritesDatabase {
   static Future<Database> open() async {
     return await openDatabase(
       // iOS、Android それぞれに適したファイル置き場のパスを返す
-      join(await getDatabasesPath(), "poke_favs.db"),
+      join(await getDatabasesPath(), favoritesFileName),
       // データベースファイルが見つからない時に呼び出される
       onCreate: (db, version) {
         return db.execute(
-          'CREATE TABLE favorites(id INTEGER PRIMARY KEY)',
+          'CREATE TABLE $favoritesTableName(id INTEGER PRIMARY KEY)',
         );
       },
       version: 1
