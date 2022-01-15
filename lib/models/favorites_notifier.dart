71d7c99be0eb3caeb9eaf4f2e2a03e4cc2b49109
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-15 21:08:22 +0900

    Update favorites_notifier.dart

--- lib/models/favorites_notifier.dart
+++ lib/models/favorites_notifier.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_sample_app/database/favorites_database.dart';
 import 'package:pokemon_sample_app/models/favorite.dart';
 
 class FavoritesNotifier extends ChangeNotifier {
   final List<Favorite> _favorites = [];
   List<Favorite> get favorites => _favorites;
+
+  FavoritesNotifier() { syncDatabase(); }
+
+  void syncDatabase() async {
+    FavoritesDatabase.read().then((value) =>
+        _favorites..clear()..addAll(value)
+    );
+    notifyListeners();
+  }
 
   void toggle(Favorite favorite) {
     isExist(favorite.pokeId) ? _delete(favorite.pokeId) : _add(favorite);
