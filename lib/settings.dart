8e4a287cad3f236b978476209b0e5e9799403cd9
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-01-24 20:50:58 +0900

    Update settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,40 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pokemon_sample_app/models/theme_mode_notifier.dart';
 import 'package:pokemon_sample_app/theme_mode_selection_page.dart';
-import 'package:provider/provider.dart';
-import './utils/theme_mode.dart';
 
-class Settings extends StatefulWidget {
+class Settings extends ConsumerWidget {
   const Settings({Key? key}) : super(key: key);
 
   @override
-  State<Settings> createState() => _SettingsState();
-}
-
-class _SettingsState extends State<Settings> {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<ThemeModeNotifier>(builder: (context, modeNotifier, child) => ListView(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+    return ListView(
       children: [
         ListTile(
           leading: const Icon(Icons.lightbulb),
           title: const Text("Dark/Light Mode"),
-          trailing: Text(getModeString(modeNotifier.mode)),
+          trailing: Text(getModeString(themeMode)),
           onTap: () async {
             // モード選択画面での結果を受け取る
             var ret = await Navigator.of(context).push<ThemeMode>(
                 MaterialPageRoute(
-                    builder: (context) => ThemeModeSelectionPage(mode: modeNotifier.mode)
+                    builder: (context) => ThemeModeSelectionPage(mode: themeMode)
                 )
             );
             // trailingテキスト更新
             if (ret != null) {
-              modeNotifier.update(ret);
+              ref.read(themeModeProvider.notifier).update(ret);
             }
           },
         )
       ],
-    ));
+    );
   }
 
   String getModeString(ThemeMode mode) {
